f68b18d19b45ca10db5dfaf97f5051538e3d61c9
Jackson_young <15190052893@163.com>
2020-08-04 09:36:11 +0800

    项目初始化

---
+++ gulimall-coupon/src/test/java/com/young/gulimall/gulimallcoupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.young.gulimall.gulimallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
